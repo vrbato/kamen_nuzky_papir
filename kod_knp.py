09a073f1ebd7165ca44fd911fd65f9b05841c1af
vrbato <246Makato624>
2022-02-27 15:16:46 +0100

    nastavení def na bodování

--- kod_knp.py
+++ kod_knp.py
@@ -1,118 +1,109 @@
 import random
-
-ODDELOVAC = "=" * 50
-MOZNOSTI = ("kamen", "nuzky", "papir")
-score_hrac = 0
-score_pc = 0
+from fce_bodovani import *
 
 
-print(
-    f"{ODDELOVAC}",
-    f"{'Pojď si zahrát hru':^50}",
-    f"{','.join(MOZNOSTI).upper():^50}",
-    f"{ODDELOVAC}",
-    sep="\n"
-)
+def main():
+    ODDELOVAC = "=" * 50
+    MOZNOSTI = ("kamen", "nuzky", "papir")
+    score_hrac = 0
+    score_pc = 0
 
-while True:
-    print(ODDELOVAC)
-    hrac = input("Tvoje volba: ").lower()
-    pocitac = random.choice(MOZNOSTI)
-    print(f"Počítač volí: {pocitac}")
+    print(
+        f"{ODDELOVAC}",
+        f"{'Pojď si zahrát hru':^50}",
+        f"{','.join(MOZNOSTI).upper():^50}",
+        f"{ODDELOVAC}",
+        sep="\n"
+    )
 
-    #ověření zadání
-    if hrac not in MOZNOSTI:
-        print("Neplatná volba!".upper())
+    while True:
+        print(ODDELOVAC)
+        hrac = input("Tvoje volba: ").lower()
+        pocitac = random.choice(MOZNOSTI)
+        print(f"Počítač volí: {pocitac}")
 
-    elif hrac in MOZNOSTI:
-        # remíza
-        if hrac == pocitac:
-            print(ODDELOVAC)
-            print(f"{'Remíza!':^50}")
-            print(ODDELOVAC)
-            print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-            print("-" * 50)
+        #ověření zadání
+        if hrac not in MOZNOSTI:
+            print("Neplatná volba!".upper())
 
-
-        #papir - kámen
-        if hrac == "papir":
-            if pocitac == "kamen":
-                vysledek = "vyhra"
+        elif hrac in MOZNOSTI:
+            # remíza
+            if hrac == pocitac:
+                vysledek = "remiza"
                 print(ODDELOVAC)
-                print(f"{'Vyhrál jsi!':^50}")
+                print(f"{'Remíza!':^50}")
                 print(ODDELOVAC)
-                if vysledek == "vyhra":
+                bodovani(vysledek,score_hrac, score_pc)
+
+            #papir - kámen
+            if hrac == "papir":
+                if pocitac == "kamen":
+                    vysledek = "vyhra"
+                    print(ODDELOVAC)
+                    print(f"{'Vyhrál jsi!':^50}")
+                    print(ODDELOVAC)
                     score_hrac += 1
-                print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-                print("-" * 50)
+                    bodovani(vysledek,score_hrac, score_pc)
 
-        elif hrac == "kamen":
-            if pocitac == "papir":
-                vysledek = "prohra"
-                print(ODDELOVAC)
-                print(f"{'Prohrál jsi!':^50}")
-                print(ODDELOVAC)
-                if vysledek == "prohra":
+            elif hrac == "kamen":
+                if pocitac == "papir":
+                    vysledek = "prohra"
+                    print(ODDELOVAC)
+                    print(f"{'Prohrál jsi!':^50}")
+                    print(ODDELOVAC)
                     score_pc += 1
-                print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-                print("-" * 50)
-                exit = input("Chceš hrát znovu? A/N: ").lower()
-                if exit != "a":
-                    print(f"{'Díky za hru':^50}")
-                    break
+                    bodovani(vysledek,score_hrac, score_pc)
+                    exit = input("Chceš hrát znovu? A/N: ").lower()
+                    if exit != "a":
+                        print(f"{'Díky za hru':^50}")
+                        break
 
-        #nuzky - kamen
-        if hrac == "kamen":
-            if pocitac == "nuzky":
-                vysledek = "vyhra"
-                print(ODDELOVAC)
-                print(f"{'Vyhrál jsi!':^50}")
-                print(ODDELOVAC)
-                if vysledek == "vyhra":
+            #nuzky - kamen
+            if hrac == "kamen":
+                if pocitac == "nuzky":
+                    vysledek = "vyhra"
+                    print(ODDELOVAC)
+                    print(f"{'Vyhrál jsi!':^50}")
+                    print(ODDELOVAC)
                     score_hrac += 1
-                print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-                print("-" * 50)
+                    bodovani(vysledek,score_hrac, score_pc)
 
-        elif hrac == "nuzky":
-            if pocitac == "kamen":
-                vysledek = "prohra"
-                print(ODDELOVAC)
-                print(f"{'Prohrál jsi!':^50}")
-                print(ODDELOVAC)
-                if vysledek == "prohra":
+            elif hrac == "nuzky":
+                if pocitac == "kamen":
+                    vysledek = "prohra"
+                    print(ODDELOVAC)
+                    print(f"{'Prohrál jsi!':^50}")
+                    print(ODDELOVAC)
                     score_pc += 1
-                print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-                print("-" * 50)
-                exit = input("Chceš hrát znovu? A/N: ").lower()
-                if exit != "a":
-                    print(f"{'Díky za hru':^50}")
-                    break
+                    bodovani(vysledek,score_hrac, score_pc)
+                    exit = input("Chceš hrát znovu? A/N: ").lower()
+                    if exit != "a":
+                        print(f"{'Díky za hru':^50}")
+                        break
 
-        #nuzky - papir
-        if hrac == "nuzky":
-            if pocitac == "papir":
-                vysledek = "vyhra"
-                print(ODDELOVAC)
-                print(f"{'Vyhrál jsi!':^50}")
-                print(ODDELOVAC)
-                if vysledek == "vyhra":
+            #nuzky - papir
+            if hrac == "nuzky":
+                if pocitac == "papir":
+                    vysledek = "vyhra"
+                    print(ODDELOVAC)
+                    print(f"{'Vyhrál jsi!':^50}")
+                    print(ODDELOVAC)
                     score_hrac += 1
-                print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-                print("-" * 50)
+                    bodovani(vysledek,score_hrac, score_pc)
 
-        elif hrac == "papir":
-            if pocitac == "nuzky":
-                vysledek = "prohra"
-                print(ODDELOVAC)
-                print(f"{'Prohrál jsi!':^50}")
-                print(ODDELOVAC)
-                if vysledek == "prohra":
+            elif hrac == "papir":
+                if pocitac == "nuzky":
+                    vysledek = "prohra"
+                    print(ODDELOVAC)
+                    print(f"{'Prohrál jsi!':^50}")
+                    print(ODDELOVAC)
                     score_pc += 1
-                print(f"Body Hráč: {score_hrac} vs Počítač: {score_pc}")
-                print("-" * 50)
-                exit = input("Chceš hrát znovu? A/N: ").lower()
-                if exit != "a":
-                    print(f"{'Díky za hru':^50}")
-                    break
+                    bodovani(vysledek,score_hrac, score_pc)
+                    exit = input("Chceš hrát znovu? A/N: ").lower()
+                    if exit != "a":
+                        print(f"{'Díky za hru':^50}")
+                        break
 
+if __name__ == "__main__":
+    main()
 
